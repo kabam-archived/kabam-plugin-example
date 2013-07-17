70544cee5aee56b2d32cb5e1ae57275fdafd0670
Anatolij Ostroumov <ostroumov095@gmail.com>
2013-07-17 22:47:39 +0400

    [feature] - more info is shown

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-module.exports = exports = function (app, routeToBind) {
+var os = require('os');
+
+module.exports = exports = function (app, routeToBind,config) {
     app.use(routeToBind, function (request, response, next) {
-            response.json({'current_time': (new Date().toLocaleString())});
-            //next(); - there is now any other things to happen latter
+            response.json(
+                {
+                    'current_time': (new Date().toLocaleString()),
+                    'NODE_ENV': process.env.NODE_ENV,
+                    'OS':{
+                        'hostname':os.hostname(),
+                        'arch':os.arch(),
+                        'type':os.type(),
+                        'platform':os.platform(),
+                        'release':os.release(),
+                        'NodeJS version':process.version
+                    }
+                });
+            //next(); - there is no any other things to happen later
         }
     )
 }
