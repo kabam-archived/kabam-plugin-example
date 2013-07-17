eed44fc102fc343421a7282d5b1df6725e956592
Anatolij Ostroumov <ostroumov095@gmail.com>
2013-07-17 22:04:03 +0400

    new syntax of initialization

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-module.exports = exports = function (request, response, next) {
-    response.json({'current_time': (new Date().toLocaleString())});
-    //next(); - there is now any other things to happen latter
+module.exports = exports = function (app, routeToBind) {
+    app.use(routeToBind, function (request, response, next) {
+            response.json({'current_time': (new Date().toLocaleString())});
+            //next(); - there is now any other things to happen latter
+        }
+    )
 }
