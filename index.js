6bf928f1f5272d32eb0875e380f0ba0fa26e318d
Anatolij Ostroumov <ostroumov095@gmail.com>
2013-07-17 20:15:24 +0400

    [fix]

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 module.exports = exports = function (request, response, next) {
-    response.json({'current_time': (new Date.toLocaleString())});
+    response.json({'current_time': (new Date().toLocaleString())});
     //next(); - there is now any other things to happen latter
 }
