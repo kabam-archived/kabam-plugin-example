63e8172443a418510be1c4882665bbe96ac30e9d
Anatolij Ostroumov <ostroumov095@gmail.com>
2013-07-18 18:45:06 +0400

    more functions in example

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var os = require('os');
 
-module.exports = exports = function (app, routeToBind,config) {
-    app.use(routeToBind, function (request, response, next) {
+module.exports = exports = function (MWC) {
+
+
+    //setting up the middleware
+    MWC.app.use('/config', function (request, response, next) {
             response.json(
                 {
                     'current_time': (new Date().toLocaleString()),
@@ -16,6 +19,54 @@ module.exports = exports = function (app, routeToBind,config) {
                     }
                 });
             //next(); - there is no any other things to happen later
+            //so, the '/example/time' will never be executed
         }
-    )
+    );
+
+    //this route is not executed, because it is overlaped by middleware above
+    MWC.app.get('/config/time',function(request,response){
+        response.send('Current time is '+(new Date().toLocaleString()));
+    });
+
+    //but this route is executed
+    MWC.app.get('/time',function(request,response){
+        response.send('Current time is '+(new Date().toLocaleString()));
+    });
+
+    //setting index
+    MWC.app.get('/',function(req,res){
+        res.send('<html>' +
+            '<head>MyWebClass Core Example</head>' +
+            '<body>' +
+            '<p>Hello, friend! You can do this things:</p><ul>' +
+            '<li>See current <a href="/time">time</a></li>' +
+            '<li>See <a href="/team">team</a> on this server</li>' +
+            '<li>See all <a href="/redis">redis</a> keys stored</li>' +
+            '<li>See this server <a href="/config">parameters</a></li>' +
+            '<li>See users <a href="/api/users">api endpoint</a> on this server</li>' +
+            '<li>See <a href="/plugins">plugins</a> installed on this server</li>' +
+            '<li>See this application <a href="https://github.com/mywebclass/mwc_plugin_example">source on Github</a></li>' +
+            '</ul></body>' +
+            '</html>');
+    });
+
+    //we use Mongoose Model in this route
+    MWC.app.get('/team',function(request,response){
+        request.MODEL.Users.find({active:1},function(err,users){
+           if(err) throw err;
+           response.json({'Team':users});
+        });
+    });
+    //we use exposed Redis client. In a rather stupid way.
+    MWC.app.get('/redis',function(request,response){
+       request.redisClient.keys('*',function(err,keys){
+           response.json(keys);
+       });
+    });
+
+    // making mechanical clock with artifical bird in it)
+    // MWC emits event of 'coocoo' with current time every 5 seconds
+    setInterval(function(){
+        MWC.emit('Coocoo!','Time now is '+(new Date().toLocaleTimeString()));
+    },5000);
 }
