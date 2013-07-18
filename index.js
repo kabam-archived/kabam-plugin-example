301df309092add83dc3354cb1db0965f7aa9842c
Anatolij Ostroumov <ostroumov095@gmail.com>
2013-07-18 19:08:13 +0400

    [feature] demonstration for request.emitMWC function

--- index.js
+++ index.js
@@ -39,12 +39,13 @@ module.exports = exports = function (MWC) {
             '<head>MyWebClass Core Example</head>' +
             '<body>' +
             '<p>Hello, friend! You can do this things:</p><ul>' +
-            '<li>See current <a href="/time">time</a></li>' +
-            '<li>See <a href="/team">team</a> on this server</li>' +
-            '<li>See all <a href="/redis">redis</a> keys stored</li>' +
-            '<li>See this server <a href="/config">parameters</a></li>' +
-            '<li>See users <a href="/api/users">api endpoint</a> on this server</li>' +
-            '<li>See <a href="/plugins">plugins</a> installed on this server</li>' +
+            '<li>See current <a href="/time">time</a>.</li>' +
+            '<li>See <a href="/team">team</a> on this server.</li>' +
+            '<li>See all <a href="/redis">redis</a> keys stored.</li>' +
+            '<li>See this server <a href="/config">parameters</a>.</li>' +
+            '<li>See users <a href="/api/users">api endpoint</a> on this server.</li>' +
+            '<li>See <a href="/plugins">plugins</a> installed on this server.</li>' +
+            '<li><a href="/honeypot">Notify</a> admin of your presense.</li>'+
             '<li>See this application <a href="https://github.com/mywebclass/mwc_plugin_example">source on Github</a></li>' +
             '</ul></body>' +
             '</html>');
@@ -62,6 +63,12 @@ module.exports = exports = function (MWC) {
        request.redisClient.keys('*',function(err,keys){
            response.json(keys);
        });
+    });
+
+    //making mousetrap - when user visits this url, MWC emmits the event
+    MWC.app.get('/honeypot',function(request,response){
+       request.emitMWC('honeypot accessed','Somebody with IP of '+request.ip+' accessed the honeypot');
+       response.send('Administrator was notified about your actions!');
     });
 
     // making mechanical clock with artifical bird in it)
