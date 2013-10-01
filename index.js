c5c393ed34602b50ea66b88ff0e80a613b8d00f0
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2013-10-02 04:34:10 +0700

    refactor: replace the mwc terms with Kabam
    NOTICE: this changes are still broken, so when you build the example you will get an error

--- index.js
+++ index.js
@@ -4,20 +4,21 @@ exports.extendApp = function(core) {
   core.app.set('Var1', '42');
 };
 
-exports.extendMiddleware = function(core) {
+/* jshint unused: vars */
+exports.extendMiddleware = function(kabam) {
   return function(request, response, next) {
-    response.setHeader('X-MWC-PLUGIN_EXAMPLE!','THIS ROCKS!');
+    response.setHeader('X-KABAM-PLUGIN_EXAMPLE!','THIS ROCKS!');
     next();
   };
 };
 
-exports.extendRoutes = function(core) {
-  core.app.get('/time',function(request, response) {
+exports.extendRoutes = function(kabam) {
+  kabam.app.get('/time',function(request, response) {
     response.send('Current time is ' + (new Date().toLocaleString()));
   });
 
 
-  core.app.get('/config', function(request, response) {
+  kabam.app.get('/config', function(request, response) {
     response.json(
       {
         'current_time': (new Date().toLocaleString()),
@@ -34,22 +35,22 @@ exports.extendRoutes = function(core) {
   });
 
   //we use Mongoose Model in this route
-  core.app.get('/team', function(request, response) {
+  kabam.app.get('/team', function(request, response) {
     request.model.Users.find({active:1}, function(err, users) {
       if (err) { throw err; }
       response.json({'Team':users});
     });
   });
   //we use exposed Redis client. In a rather stupid way.
-  core.app.get('/redis', function(request, response) {
+  kabam.app.get('/redis', function(request, response) {
     request.redisClient.keys('*',function(err, keys) {
       response.json(keys);
     });
   });
 
-  //making mousetrap - when user visits this url, MWC emmits the event
-  core.app.get('/honeypot', function(request, response) {
-    request.emitMWC('honeypot accessed','Somebody with IP of ' + request.ip + ' accessed the honeypot');
+  //making mousetrap - when user visits this url, Kabam emmits the event
+  kabam.app.get('/honeypot', function(request, response) {
+    request.emitKabam('honeypot accessed','Somebody with IP of ' + request.ip + ' accessed the honeypot');
     response.send('Administrator was notified about your actions!');
   });
 };
